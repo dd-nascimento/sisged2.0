f81468bb258a14181b2b52b0bd5a498d50bd58fa
David Nascimento <dnmnascimento@outlook.com>
2019-02-25 03:30:21 -0300

    Conclusão do cadastro de secretaria

--- Cadstro-sec.php
+++ Cadstro-sec.php
@@ -33,7 +33,7 @@
                         <li class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle mr-4" data-toggle="dropdown" id="navDrop"> CADASTROS </a>
                                 <div class="dropdown-menu">
-                                        <a href="#" class="dropdown-item"> Secretarias </a>
+                                        <a href="Cadstro-sec.php" class="dropdown-item"> Secretarias </a>
                                         <a href="#" class="dropdown-item"> Colaborador(a).  </a>
                                 </div>
                         </li>
@@ -53,9 +53,9 @@
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navDrop"> Institucionais </a>
                         
                             <div class="dropdown-menu">
-                                    <a href="#" class="dropdown-item"> Prefeitura </a>
-                                    <a href="#" class="dropdown-item"> Secretaria de Educação  </a>
-                                    <a href="#" class="dropdown-item"> Governo do Estado  </a>
+                                    <a href="https://www.guarulhos.sp.gov.br" target="blank" class="dropdown-item"> Prefeitura </a>
+                                    <a href="https://portaleducacao.guarulhos.sp.gov.br/portal/" target="blank" class="dropdown-item"> Secretaria de Educação  </a>
+                                    <a href="https://www.saopaulo.sp.gov.br" target="blank" class="dropdown-item"> Governo do Estado  </a>
                             </div>
                         </li>
                     </ul>
@@ -74,7 +74,7 @@
                                     <label for="text" class="nomesec"> Nome do(a) Secretaria/Departamento.</label> 
                                         <input type="text" class="form-control" placeholder="Nome da Secretaria ou Departamento.">
                                             <br>
-                                            <button type="submit" class="btn btn-primary">Cadastrar </button> | <button type="reset" class="btn btn-danger">Limpar</button>
+                                            <button type="submit" class="btn btn-success">Cadastrar </button> | <button type="reset" class="btn btn-warning">Limpar</button>
                     </form>
                 </div>
     </div>
